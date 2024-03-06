9f7c80e4fe3f9eaa8b389106b5edb9eabf1fc3c3
mutiaauliakalsum <mutiaauliakalsum05@gmail.com>
2024-03-06 13:15:17 +0700

    first commit

---
+++ pertemuan3/form.php
@@ -0,0 +1,37 @@
+<?php
+    include_once 'atas.php';
+?>
+
+<div class="container my-5">
+            <h1 class="text-center mb-4">Triangle Area Calculator</h1>
+            <form action="form.php" method="post">
+                <div class="form-floating mb-3">
+                    <label for="base">Base</label>
+                    <input type="number" class="form-control" id="base" name="base" required>
+                </div>
+                <div class="form-floating mb-3">
+                    <label for="height">Height</label>
+                    <input type="number" class="form-control" id="height" name="height" required>
+                </div>
+                <button name="submit" type="submit" class="btn btn-primary">Calculate Area</button>
+            </form>
+        </div>
+
+<?php
+        require_once 'libfungsi.php';
+        $base = $_POST['base'];
+        $height = $_POST['height'];
+        $area = hitungluas($base, $height);
+        $type = deteksisegitiga($base, $height);
+?>
+
+        <div class="mt-4">
+            <h2>hasil:</h2>
+            <p>Alas: <?= $base ?> cm</p>
+            <p>Tinggi: <?= $height ?> cm</p>
+            <p>Luas: <?= $area ?> cm</p>
+            <p>Jenis: <?= $type ?></p>
+        </div>
+
+
+  
